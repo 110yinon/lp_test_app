c709d611c83132f02b74eca23069fa14f4d117a2
yinon <yinon.barhun@litepoint.com>
2023-02-15 14:14:42 +0200

    installValidate.js is working

--- installValidate.js
+++ installValidate.js
@@ -1,13 +1,29 @@
 const fsPromise = require('node:fs/promises');
+const util = require('util');
+const execFile = util.promisify(require('child_process').execFile);
 
-async function installValidate(dirPath) {
 
+async function installValidate(exePath) {
+
+    //path to install the IQDVT
+    const appPath = 'C:\\IQDVT_TEST\\';
+
+    // automated installation
+    try {
+        const std = await execFile(exePath, ['/S', `/D=${appPath}`]);
+        console.log(std.stout);
+        console.log(std.stderr);
+    }
+    catch (err) {
+        console.log('err:', err);
+    }
+
+    // checks for files exist in directory
     const filesToCheck = ['IQTest.dll', 'IQTestAPI.dll', 'IQDVT.exe', 'IQDVT-CLI.exe'];
     let isFilesIncludes = false;
 
-    const files = await fsPromise.readdir(dirPath);
-
-    // checks for files exist in directory
+    const files = await fsPromise.readdir(`${appPath}/Bin`);
+    
     isFilesIncludes = filesToCheck.every(file => {
         return files.includes(file);
     });

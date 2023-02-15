1b4fc680a4fa545b858ed93beddd9dc05bfbfdb1
yinon <yinon.barhun@litepoint.com>
2023-02-15 14:16:21 +0200

    update the installValidate.test.js file

--- installValidate.test.js
+++ installValidate.test.js
@@ -2,5 +2,8 @@ const installValidate = require('./installValidate');
 
 
 
-installValidate('C:/LitePoint/IQDVT-CL_8XXX-Temp-14-02-2023/IQDVT-CL_8XXX-Temp-14-02-2023_1.0.9/Bin')
+installValidate('C:/Users/ybarhum/Downloads/IQDVT-CL_8XXX-Temp-14-02-2023_1.0.9_x64.exe')
     .then(installed => console.log('~~ Installed correctly:', installed))
+
+// installValidate('')
+//     .then(installed => console.log('~~ Installed correctly:', installed))

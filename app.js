9e35b0d9f396a5850c2e2d1c365a66e78e2fc861
yinon <yinon.barhun@litepoint.com>
2023-02-19 17:48:29 +0200

    switch flows and sample to PASS/FAILED is working

--- app.js
+++ app.js
@@ -9,41 +9,24 @@ const { spawn } = require('node:child_process');
 // const { execFileSync } = require('node:child_process');
 
 const appPath = 'C:\\IQDVT_TEST\\';
-const iqdvtCli = `${appPath}\\Bin\\IQDVT-CLI.exe`;
-const flowDir = 'c:\LitePoint\flows\celeno_flow_16_02_2023.flow';
-const stationDir = 'c:\LitePoint\stations\celeno_16_02_2023.sta';
-'IQDVT-CLI.exe --flow=c:\\LitePoint\\flows\\celeno_flow_16_02_2023.flow --station=c:\\LitePoint\\stations\\celeno_16_02_2023.sta'
-// console.log('a');
-paramArr = ['--v1','--flow=c:\\LitePoint\\flows\\celeno_flow_16_02_2023.flow', '--station=c:\\LitePoint\\stations\\celeno_16_02_2023.sta']
-const ef = execFile('C:\\IQDVT_TEST\\Bin\\IQDVT-CLI.exe', paramArr, {'cwd':'C:\\IQDVT_TEST\\Bin'}, (err, stdout, stderr) => {
-    console.log(stdout);
-    console.log(stdout.includes('PASS'));
+const flowDir = 'c:\\LitePoint\\flows\\Flow.flow';
+const flowDir1 = 'c:\\LitePoint\\flows\\celeno_flow_16_02_2023.flow';
+const stationDir = 'c:\\LitePoint\\stations\\celeno_16_02_2023.sta';
+
+
+paramArr = ['--v1', `--station=${stationDir}`, `--flow=${flowDir1}`];
+const ef = execFile('C:\\IQDVT_TEST\\Bin\\IQDVT-CLI.exe', paramArr, { 'cwd': 'C:\\IQDVT_TEST\\Bin' }, (err, stdout, stderr) => {
+
+    const [, testsSummary] = stdout.split('Summary:');
+
+    // console.log('is connected to DUT:', stdout.includes('Connect finished successfully'));
+    console.log('is test pass:', !testsSummary.includes('FAILED'));
 })
 
-// ef.stdout.on('data', data => {
-//     console.log(data.toString());
-// });
-// console.log('b');
+// fs.readdir('c:/IQDVT_TEST/Bin', (err, files) => {
+//     console.log('e');
 
-// setTimeout(() => {
-//     fs.readdir('c:/', (err, files) => {
-//         console.log('e');
+//     console.log(files, files.length);
+//     console.log('f');
 
-//         console.log(files);
-//         console.log('f');
-
-//     });
-// }, 0)
-
-// console.log('c');
-
-// ef.on('close', (code) => {
-//     console.log(`child process close all stdio with code ${code}`);
-//     fs.readdir('c:/IQDVT_TEST/Bin', (err, files) => {
-//         console.log('e');
-
-//         console.log(files,files.length);
-//         console.log('f');
-
-//     });
 // });

a7d93d06f8a4e993e79fd2a381698232b549fff9
yinon <yinon.barhun@litepoint.com>
2023-03-01 11:32:41 +0200

    added flow and station params to flow&station test

--- suites/flow&station.test.js
+++ suites/flow&station.test.js
@@ -5,7 +5,7 @@ const flowANDstationValidate = require('../tests/flow&station');
 
 (async () => {
     console.log('1');
-    const res = await flowANDstationValidate();
+    const res = await flowANDstationValidate('celeno_16_02_2023.sta', 'txCal.flow');
     console.log('result:', res);
     console.log('2');
 })();
--- tests/flow&station.js
+++ tests/flow&station.js
@@ -1,10 +1,32 @@
 const util = require('util');
 const execFile = util.promisify(require('child_process').execFile);
+const path = require('path');
 
 
-async function flowANDstationValidate() {
-    const stationDir = 'c:\\LitePoint\\stations\\celeno_16_02_2023.sta';
-    const flowDir = 'c:\\LitePoint\\flows\\txCal.flow';
+async function flowANDstationValidate(...flags) {
+
+    // 'no-order' feature - we can put flow | station first - and it works
+    const [station] = flags.filter(flag => flag.includes('.sta'));
+    const [flow] = flags.filter(flag => flag.includes('.flow'));
+    if (!station) {
+        console.log("no '.sta' file");
+        return false;
+    }
+    if (!flow) {
+        console.log("no '.flow' file");
+        return false;
+    }
+
+
+    // make sure we always get the c:\........\test - main folder
+    // though we run from runner.test.js | flow&station.test.js
+    const testMainDir = path.resolve(__dirname.split('test - main')[0], 'test - main');
+
+    const stationDir = path.resolve(testMainDir, 'stations', station);
+    const flowDir = path.resolve(testMainDir, 'flows', flow);
+
+    console.log('stationDir:', stationDir);
+    console.log('flowDir:', flowDir);
 
     const paramArr = ['--v1', `--station=${stationDir}`, `--flow=${flowDir}`];
     // const paramArr = ['--v1'];

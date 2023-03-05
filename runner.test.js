6a5b9a742308dd5386c1d447fa3e67a1e6500e6a
yinon <yinon.barhun@litepoint.com>
2023-03-05 13:02:53 +0200

    split to sync and async versions of the flow&station test

--- runner.test.js
+++ runner.test.js
@@ -2,30 +2,97 @@ const helpFlagValidate = require('./tests/helpFlag');
 const installValidate = require('./tests/installValidate');
 const uninstallValidate = require('./tests/uninstallValidate');
 const flowANDstationValidate = require('./tests/flow&station');
+const flowANDstationValidateSync = require('./tests/flow&StationSync');
 const configFile = require('./config/config.runner.json');
 
-(
-    async () => {
-        console.log(configFile.tests);
-        configFile.tests.forEach(async (test, index) => {
-            switch (test.command.toLowerCase()) {
-                case 'cli':
-                    console.log(index);
-                    let res = await flowANDstationValidate(test.station, test.flow);
-                    console.log('index:', index, test.comment, res === test.expect);
+// (
+//     () => {
+//         // SYNC VERSION - forEach - WORKS ALWAYS
+//         const start = Date.now();
 
-                    break;
-                default:
-                    break;
-            }
-        });
+//         configFile.tests.forEach((test, index) => {
+//             switch (test.command.toLowerCase()) {
+//                 case 'cli':
+//                     console.log(index);
+//                     let res = flowANDstationValidateSync(test.station, test.flow);
+//                     console.log('index:', index, test.comment, res === test.expect);
+//                     break;
+//                 default:
+//                     break;
+//             }
+//         });
+//         console.log('time elapsed:', Date.now() - start);
+//     }
+// )();
 
 
-        // res = await flowANDstationValidate('celeno_16_02_2023.sta', 'iterate.flow');
-        // console.log("~~ iterate 1", res);
 
-        // res = await flowANDstationValidate('celeno_16_02_2023.sta', 'iterate.flow');
-        // console.log("~~ iterate 2", res);
 
-    }
-)();
+// (
+//     () => {
+//         // SYNC VERSION - WORKS ALWAYS
+//         const start = Date.now();
+//         let res = flowANDstationValidateSync(configFile.tests[0].station, configFile.tests[0].flow);
+//         console.log('index:', 0, configFile.tests[0].comment, res === configFile.tests[0].expect);
+
+//         let res1 = flowANDstationValidateSync(configFile.tests[1].station, configFile.tests[1].flow);
+//         console.log('index:', 1, configFile.tests[1].comment, res1 === configFile.tests[1].expect);
+
+//         let res2 = flowANDstationValidateSync(configFile.tests[2].station, configFile.tests[2].flow);
+//         console.log('index:', 2, configFile.tests[2].comment, res2 === configFile.tests[2].expect);
+
+//         let res3 = flowANDstationValidateSync(configFile.tests[3].station, configFile.tests[3].flow);
+//         console.log('index:', 3, configFile.tests[3].comment, res3 === configFile.tests[3].expect);
+
+//         let res4 = flowANDstationValidateSync(configFile.tests[4].station, configFile.tests[4].flow);
+//         console.log('index:', 4, configFile.tests[4].comment, res4 === configFile.tests[4].expect);
+//         console.log('time elapsed:', Date.now() - start);
+//     }
+// )();
+
+
+
+
+// (
+//     async () => {
+//         //ASYNC VERSION - iterate - WORKS ALTERNATLY
+//         const start = Date.now();
+//         console.log(configFile.tests);
+//         configFile.tests.forEach(async (test, index) => {
+//             switch (test.command.toLowerCase()) {
+//                 case 'cli':
+//                     console.log(index);
+//                     let res = await flowANDstationValidate(test.station, test.flow);
+//                     console.log('index:', index, test.comment, res === test.expect);
+//                     console.log('time elapsed:', Date.now() - start);
+//                     break;
+//                 default:
+//                     break;
+//             }
+//         });
+//     }
+// )();
+
+
+// (
+//     async () => {
+//         //ASYNC VERSION - WORKS ALTERNATLY
+//         const start = Date.now();
+//         let res = await flowANDstationValidate(configFile.tests[0].station, configFile.tests[0].flow);
+//         console.log('index:', 0, configFile.tests[0].comment, res === configFile.tests[0].expect);
+
+//         let res1 = await flowANDstationValidate(configFile.tests[1].station, configFile.tests[1].flow);
+//         console.log('index:', 1, configFile.tests[1].comment, res1 === configFile.tests[1].expect);
+
+//         let res2 = await flowANDstationValidate(configFile.tests[2].station, configFile.tests[2].flow);
+//         console.log('index:', 2, configFile.tests[2].comment, res2 === configFile.tests[2].expect);
+
+//         let res3 = await flowANDstationValidate(configFile.tests[3].station, configFile.tests[3].flow);
+//         console.log('index:', 3, configFile.tests[3].comment, res3 === configFile.tests[3].expect);
+
+//         let res4 = await flowANDstationValidate(configFile.tests[4].station, configFile.tests[4].flow);
+//         console.log('index:', 4, configFile.tests[4].comment, res4 === configFile.tests[4].expect);
+//         console.log('time elapsed:', Date.now() - start);
+
+//     }
+// )();
---
+++ tests/flow&StationSync.js
@@ -0,0 +1,62 @@
+const util = require('util');
+const execFile = util.promisify(require('child_process').execFile);
+const path = require('path');
+
+const { execFileSync } = require('child_process');
+
+function flowANDstationValidate(...flags) {
+
+    // 'no-order' feature - we can put flow | station first - and it works
+    const [station] = flags.filter(flag => flag.includes('.sta'));
+    const [flow] = flags.filter(flag => flag.includes('.flow'));
+    if (!station) {
+        console.log("no '.sta' file");
+        return false;
+    }
+    if (!flow) {
+        console.log("no '.flow' file");
+        return false;
+    }
+
+
+    // make sure we always get the c:\........\test - main folder
+    // though we run from runner.test.js | flow&station.test.js
+    const testMainDir = path.resolve(__dirname.split('test - main')[0], 'test - main');
+
+    const stationDir = path.resolve(testMainDir, 'stations', station);
+    const flowDir = path.resolve(testMainDir, 'flows', flow);
+
+    // console.log('stationDir:', stationDir);
+    // console.log('flowDir:', flowDir);
+
+    const paramArr = ['--v1', `--station=${stationDir}`, `--flow=${flowDir}`];
+    // const paramArr = ['--v1'];
+
+    // const { stdout, stderr } = await execFile('IQDVT-CLI.exe', paramArr, { cwd: 'C:\\IQDVT_TEST\\Bin', encoding: 'utf8' });
+    try {
+        const stdout = execFileSync('IQDVT-CLI.exe', paramArr, { cwd: 'C:\\IQDVT_TEST\\Bin', encoding: 'utf8' });
+
+        // console.log('stdout:', stdout);
+
+
+        // gets the test summray section from the output:
+        const [, testsSummary] = stdout.split('Summary:');
+
+        // for Older version where startupFiles is must:
+        // case of no startupFiles.json | startupFiles.json is empty
+        if (!testsSummary) {
+            // console.log('is test pass:', false);
+            return false;
+        }
+        // checks for failed tests on the tests summray:
+        // console.log('is test pass:', !testsSummary.includes('FAILED'));
+        return !testsSummary.includes('FAILED');
+    }
+    catch (stderr) {
+        console.log('stderr', stderr);
+        return false;
+    }
+}
+
+
+module.exports = flowANDstationValidate;

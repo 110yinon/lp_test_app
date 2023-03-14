6478a942bb6e13631bf64687fc63178a3f57f4a2
yinon <yinon.barhun@litepoint.com>
2023-03-14 17:01:33 +0200

    correct the flow path argument's function according to the CL-8 subfolder on the flows&stationSync.test.js file

--- suites/flow&stationSync.test.js
+++ suites/flow&stationSync.test.js
@@ -5,7 +5,7 @@ const flowANDstationValidateSync = require('../tests/flow&StationSync');
 
 (() => {
     const isBinFolder = true;
-    const isPass = flowANDstationValidateSync(isBinFolder, 'celeno_16_02_2023.sta', 'txCal.flow');
+    const isPass = flowANDstationValidateSync(isBinFolder, 'celeno_16_02_2023.sta', './CL-8 flows/11axRx.flow');
     console.log('cli flow run is pass:', isPass);
 })();
 
